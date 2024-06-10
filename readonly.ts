6969972e8ab3210fee8adf5ab5463e3061de5eda
moonhr <moonhr153@gmail.com>
2024-06-10 14:46:25 +0900

    readonly 읽기전용

--- readonly.ts
+++ readonly.ts
@@ -3,5 +3,7 @@ let user:{
 } = {
   firstName: 'abby'
 }
-user.firstName
-user.firstName = 'abbey with an e' //읽기전용 프로퍼티로 할당불가
+user.firstName //string
+//! user.firstName = 'abbey with an e' //읽기전용 프로퍼티로 할당불가
+
+//가능한 빈객체는 지양함
