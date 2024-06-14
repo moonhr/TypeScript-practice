209f509e0afcbe95a4d4ca0f4f35f43bdd417fa2
moonhr <moonhr153@gmail.com>
2024-06-14 15:57:29 +0900

    thisType 모르겠음

---
+++ generator.ts
@@ -0,0 +1,2 @@
+//* generator function
+//* 여러개의 값을 생산하는 편리한 기능 제공
--- thisType.ts
+++ thisType.ts
@@ -1,4 +1,6 @@
 //날짜의타입을 포매팅하는 유틸리티 함수
-function fancyDate() {
+function fancyDate(this : Date) {
   return ${this.getDate()}/${this.getMonth()}/${this.getFullYear()}
 }
+fancyDate.call(new Date)
+fancyDate()
